735237cd5a04a8228f8ec670ce7aafaf4672ce38
G3org3B <vbt.george@gmail.com>
2018-11-26 19:16:44 +0100

    complete

--- ex-1/main.js
+++ ex-1/main.js
@@ -5,10 +5,12 @@
 // ha visto precedentemente. Dopo che sono stati inseriti i 5 numeri, il software
 // dice quanti e quali dei numeri da indovinare sono stati individuati.
 
-
+//----------- FUNZIONE numero random---------
 function numberRandom(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
+
+//----------- Creo i 5 numeri casuali da mostrare al giocatore --------- ***
 
 var numeriCasuali = [];
 
@@ -20,22 +22,42 @@ for (var i = 0; i < 5; i++) {
   numeriCasuali.push(numeri);
 }
 
+//----------- Mostro tramite un allert le istruzioni al giocatore e i numeri da memorizzare ---------
 
-alert('Memorizzare i seguenti numeri, hai 30 secondi di tempo...' + '    ' + numeriCasuali);
+alert('Memorizza i seguenti numeri,ricorda che hai 30 secondi di tempo... dopo l\'ok' + '    ' + numeriCasuali);
+
 
 console.log(numeriCasuali);
 
 var dbnumeriUtente = [];
 
+//----------- creo la funzione che dopo 30 secondi fà inserire al giocatore i numeri memorizzati in precedenza   ---------
 
-setTimeout(timer, 3000);
-function timer() {
+setTimeout(richiestaNumeri, 3000);
+function richiestaNumeri() {
 
   for (var i = 0; i < 5; i++) {
 
     var numeriUtente = parseInt(prompt('Inserire i numeri memorizzati poco fà!'));
-    dbnumeriUtente.push(numeriUtente);
+
+    // Se dentro i numeri casuali ci sono numeri inseriti dall'utente li inserisco nel dbnumeriUtente
+
+    if (numeriCasuali.includes(numeriUtente)) {
+      dbnumeriUtente.push(numeriUtente);
+
+    }
 
   }
   console.log(dbnumeriUtente);
+
+  // Se l'utente ha indovinato almeno 1 numero ha vinto
+
+  if (dbnumeriUtente.length > 0) {
+
+    alert('Bravo ti sei ricordato questi numeri'+ '    ' + dbnumeriUtente)
+
+  } else {
+
+    alert('Non ti sei ricordato nessun numero')
+  }
 }
