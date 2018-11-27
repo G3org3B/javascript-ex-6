ffe55a6afb2f5a04cffadf6e4640400d221c1c55
G3org3B <vbt.george@gmail.com>
2018-11-27 18:30:23 +0100

    fix commenti

--- ex-2/main.js
+++ ex-2/main.js
@@ -12,18 +12,25 @@ var sesso = prompt('Sei un Uomo o sei una Donna? ');
 console.log(nome);
 console.log(sesso);
 
+// porto a lettere minuscule tutte le lettere
+
 sesso = sesso.toLowerCase();
+console.log(sesso);
 
 var identità = document.getElementById('nomeSaluto');
-console.log(identità);
 
 if (sesso == 'uomo') {
+
+  // Se sesso è maschile allora text blue
 
   nomeSaluto.style.color = '#00C9FF';
   identità.innerHTML = 'Hi !!!  ' + nome;
 
 }
  else if (sesso == 'donna') {
+
+   // Se sesso è femminile allora text blue
+
 
   nomeSaluto.style.color = 'pink';
   identità.innerHTML = 'Hi !!!  ' + nome;
